1a32ebb7088f2571d632334c7e4bc917d0f65035
lkmarcum <50113705+lkmarcum@users.noreply.github.com>
2019-08-26 14:58:12 -0500

    added getUser function to App.js

--- expat-journal/src/App.js
+++ expat-journal/src/App.js
@@ -46,6 +46,17 @@ function App() {
       });
   };
 
+  const getUser = id => {
+    axiosWithAuth()
+      .get(`******ADD ENPOINT HERE******`)
+      .then(res => {
+        console.log("GET USER res: ", res);
+      })
+      .catch(err => {
+        console.log(err.response);
+      });
+  };
+
   return (
     <PostsContext.Provider value={{ allPosts }}>
       <UserContext.Provider value={{ user, addPost, removePost, editPost }}>

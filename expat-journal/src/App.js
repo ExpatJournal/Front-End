faaccb28d4e9c9dd1547777613fe8ea16070785a
lkmarcum <50113705+lkmarcum@users.noreply.github.com>
2019-08-26 12:56:59 -0500

    add basic CRUD functions to App.js

--- expat-journal/src/App.js
+++ expat-journal/src/App.js
@@ -1,11 +1,47 @@
 import React, { useState, useEffect } from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import axiosWithAuth from "./utils/axiosWithAuth";
 
 function App() {
   const [userPosts, setUserPosts] = useState([]);
+  const [allPosts, setAllPosts] = useState([]);
 
-  const addPost = post => {};
+  const addPost = post => {
+    axiosWithAuth()
+      .post(`******ADD ENPOINT HERE******`, post)
+      .then(res => {
+        console.log("POST res: ", res);
+      })
+      .catch(err => {
+        console.log(err.response);
+      });
+  };
+
+  const removePost = (id, e) => {
+    e.stopPropagation();
+    axiosWithAuth()
+      .delete(`******ADD ENPOINT HERE******`)
+      .then(res => {
+        console.log("DELETE res: ", res);
+      })
+      .catch(err => {
+        console.log(err.response);
+      });
+  };
+
+  const editPost = (editedPost, e) => {
+    e.preventDefault();
+    axiosWithAuth()
+      .put(`******ADD ENPOINT HERE******`, editedPost)
+      .then(res => {
+        console.log("PUT res: ", res);
+      })
+      .catch(err => {
+        console.log(err.response);
+      });
+  };
+
   return (
     <div className="App">
       <header className="App-header">

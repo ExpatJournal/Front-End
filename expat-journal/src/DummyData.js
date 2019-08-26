31c039253184ce9adeecc12af2cfab94af46bb3a
Greg Johnston <GCJohnston91@gmail.com>
2019-08-26 16:56:09 -0400

    Dummy data updated with location

--- expat-journal/src/DummyData.js
+++ expat-journal/src/DummyData.js
@@ -1,18 +1,22 @@
 const DummyData = [
     {username: "YoungEnglish",
     content: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+    location: 'Istanbul'
     imgURL: 'https://image.shutterstock.com/image-photo/europe-travel-vacation-fun-summer-600w-1048935941.jpg'},
 
     {username: "KevinBacon",
     content: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+    location: 'Vietnam'
     imgURL: 'https://image.shutterstock.com/image-photo/europe-travel-vacation-fun-summer-600w-1048935941.jpg'},
 
     {username: "AppleFan123",
     content: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+    location: 'Seoul'
     imgURL: 'https://image.shutterstock.com/image-photo/young-happy-tourists-sightseeing-city-600w-651937891.jpg'},
 
     {username: "Rickay",
     content: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+    location: 'Mother Russia'
     imgURL: 'https://image.shutterstock.com/image-photo/cheerful-woman-wanderer-trendy-look-600w-360572705.jpg'},
   ]
 

92c907191128622ff45e104a243be4942387b58d
lkmarcum <50113705+lkmarcum@users.noreply.github.com>
2019-08-29 15:00:02 -0500

    updated mobile styles for EditPost component

--- expat-journal/src/components/EditPost.js
+++ expat-journal/src/components/EditPost.js
@@ -1,10 +1,12 @@
 import React, { useState, useEffect, useContext } from "react";
 import axios from "axios";
 import { UserContext } from "../contexts/UserContext";
+import MobileHeader from "./MobileHeader";
 
 const EditPost = props => {
   console.log("edit props: ", props);
   const [updatedPost, setUpdatedPost] = useState({});
+  const [imgURL, setImgURL] = useState("");
   const { editPost } = useContext(UserContext);
 
   useEffect(() => {
@@ -17,6 +19,7 @@ const EditPost = props => {
       .then(res => {
         console.log("getPost res: ", res);
         setUpdatedPost(res.data);
+        setImgURL(res.data.media[0].url);
       })
       .catch(err => {
         console.log(err.response);
@@ -33,12 +36,13 @@ const EditPost = props => {
 
   return (
     <div className="edit-page">
+      <MobileHeader />
       <h3>Edit Post</h3>
       <div className="edit-container">
-        <img src="" />
+        <img src={imgURL} />
         <div className="edit-text">
           <h4>Update Caption</h4>
-          <input
+          <textarea
             type="text"
             name="post"
             value={updatedPost.post}
@@ -49,8 +53,10 @@ const EditPost = props => {
         </div>
       </div>
       <div className="edit-buttons">
-        <button onClick={sendUpdate}>Update</button>
-        <button>Cancel</button>
+        <button className="update" onClick={sendUpdate}>
+          Update
+        </button>
+        <button className="cancel">Cancel</button>
       </div>
     </div>
   );

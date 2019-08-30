21bdf77e41eff9bc96c8a0905aa7df05a2655e5d
lkmarcum <50113705+lkmarcum@users.noreply.github.com>
2019-08-29 22:03:16 -0500

    header added to Feed page

--- expat-journal/src/components/Feed.js
+++ expat-journal/src/components/Feed.js
@@ -3,29 +3,32 @@ import { PostsContext } from "../contexts/PostsContext";
 import FeedCard from "./FeedCard";
 import HamburgerNav from "./HamburgerNav";
 import { Link } from "react-router-dom";
+import FeedHeader from "./FeedHeader";
 
 const Feed = () => {
   const { allPosts } = useContext(PostsContext);
   // console.log('DummyData', DummyData);
 
   return (
-    <div className="explore-header">
-      <HamburgerNav />
-      <p>Explore</p>
-      <div className="feed-wrapper">
-        {allPosts.map(post => (
-          <Link className="link-clear" to={`/PhotoPage/${post.id}`}>
-            <FeedCard
-              // user={post.username}
-              content={post.post}
-              location={post.location}
-              img={post.media[0].url}
-              key={post.id}
-            />
-          </Link>
-        ))}
+    <>
+      <FeedHeader />
+      <div className="explore-header">
+        <p>Explore</p>
+        <div className="feed-wrapper">
+          {allPosts.map(post => (
+            <Link className="link-clear" to={`/PhotoPage/${post.id}`}>
+              <FeedCard
+                // user={post.username}
+                content={post.post}
+                location={post.location}
+                img={post.media[0].url}
+                key={post.id}
+              />
+            </Link>
+          ))}
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 export default Feed;
